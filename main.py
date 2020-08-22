f2b4fa39f9a87e51c7a2dc461615abf841a26525
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-08-23 05:26:15 +0530

    find volatility between stock high and low prices

--- main.py
+++ main.py
@@ -4,5 +4,15 @@ import quandl
 
 df = quandl.get('WIKI/GOOGL')
 
-print(df.head())
+
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100
+
+
+
+print(df.head(10))
+
+
+
+
 
