35f2f9f5b335f9032c614d566055e801811b1c36
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-08-23 08:38:31 +0530

    create feature and labels

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import quandl
-import math
-
-
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.model_selection import cross_validate
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -22,4 +23,10 @@ forecast_out = int(math.ceil(0.01 * len(df))) #finding 1% of the days
 df['label'] = df[forecast_col].shift(-forecast_out) #shift changes the position of rows down, negative value will take the rows up. Basically we want to see what was the value of adj. close after 1% of the days for the given features.
 
 
+X = np.array(df.drop(['label'], 1)) #this is the feature column and it consists of everything except the 'label' column.
+y = np.array(df['label']) #labels column
 
+print(X)
+X = preprocessing.scale(X)
+print('xxxxxxxxxxxxxxxxxxxxxxx')
+print(X)
